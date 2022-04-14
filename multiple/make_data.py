4046ef3df83dc3a68324f10f61555bb1f9b3bf56
Gillen Brown <gillenb@umich.edu>
2022-04-14 17:42:27 -0400

    better file formatting

--- multiple/make_data.py
+++ multiple/make_data.py
@@ -5,4 +5,4 @@ n_data_files = 100
 
 for i in range(n_data_files):
     data = np.random.normal(0, 1, 1000)
-    np.savetxt(f"data/data_{i}.txt", data)
+    np.savetxt(f"data/data_{i:02}.txt", data)
