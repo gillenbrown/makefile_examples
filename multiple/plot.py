88ecfd274728384314c9e8f650f0c3bb3a5e1805
Gillen Brown <gillenb@umich.edu>
2022-04-14 17:30:14 -0400

    changed arguments of multiple plot script

--- multiple/plot.py
+++ multiple/plot.py
@@ -2,18 +2,23 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-data_files = sys.argv[1:]
-
+mean_files = []
+spread_files = []
+for f in sys.argv[1:]:
+    if "mean_" in f:
+        mean_files.append(f)
+    elif "spread_" in f:
+        spread_files.append(f)
 
 fig, ax = plt.subplots()
-for y, df in enumerate(data_files):
-    mean_file = df.replace("data_", "mean_")
-    spread_file = df.replace("data_", "spread_")
-
+y = 0
+for mean_file, spread_file in zip(mean_files, spread_files):
     mean = np.loadtxt(mean_file)
     spread = np.loadtxt(spread_file)
 
     ax.errorbar(mean, y, xerr=spread, fmt=".", c="k")
+
+    y += 1
 
 ax.set_xlabel("Value")
 ax.set_ylabel("Data File Number")
